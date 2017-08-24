66644e0729bf12ce12bdc86bd533c050428d6692
Thibstars <thibault.helsmoortel@gmail.com>
2017-08-24 13:44:46 +0200

    Add support for a property argument in order to show only given properties

--- src/main/java/be/thibaulthelsmoortel/clapp/model/defaults/SysPropsCommand.java
+++ src/main/java/be/thibaulthelsmoortel/clapp/model/defaults/SysPropsCommand.java
@@ -3,6 +3,7 @@ package be.thibaulthelsmoortel.clapp.model.defaults;
 import be.thibaulthelsmoortel.clapp.annotations.RegisterCommand;
 import be.thibaulthelsmoortel.clapp.model.Command;
 import be.thibaulthelsmoortel.clapp.view.CLTextArea;
+import org.apache.commons.collections.CollectionUtils;
 
 /**
  * Command for displaying system properties.
@@ -16,8 +17,20 @@ public class SysPropsCommand extends Command {
         super("sysProps");
         setCallable(() -> {
             StringBuilder output = new StringBuilder();
-            // TODO: 22/08/2017 Add support for a property argument in order to show only 1 property
-            System.getProperties().forEach((o, o2) -> clTextArea.append(o.toString() + ": " + o2.toString() + "\n"));
+            if (CollectionUtils.isNotEmpty(getArgs())) {
+                for (String arg : getArgs()) {
+                    String result = arg + ": " + System.getProperty(arg) + "\n";
+                    output.append(result);
+                    clTextArea.append(result);
+                }
+            } else {
+                System.getProperties().forEach((o, o2) -> {
+                    String result = o.toString() + ": " + o2.toString() + "\n";
+                    output.append(result);
+                    clTextArea.append(result);
+                });
+            }
+
             return output;
         });
     }
